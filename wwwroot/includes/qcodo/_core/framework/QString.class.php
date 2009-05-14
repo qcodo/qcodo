c43a75afe361bb6ecabb649fcb073c86267fecd9
Mike Ho <mike@quasidea.com>
2009-05-14 12:56:52 -0700

    Added reg-exp capable strpos and strrpos functionality

--- wwwroot/includes/qcodo/_core/framework/QString.class.php
+++ wwwroot/includes/qcodo/_core/framework/QString.class.php
@@ -89,5 +89,66 @@
 			$strEmail = str_replace('.', '<strong style="display: none;">' . md5(microtime()) . '</strong>&#046;<strong style="display: none;">' . md5(microtime()) . '</strong>', $strEmail);
 			return $strEmail;
 		}
+
+		/**
+		 * Similar to strpos(haystack, needle, [offset]) except "needle" can be a regular expression as well.
+		 * Will only work if both the first and last character of "needle" is "/", signifying a regexp-based search.
+		 *
+		 * NOTE: If a regexp was used, needle WILL be modified to reflect the actual string literal found/used in the search.
+		 * 
+		 * @param string $strHaystack the contents to search through
+		 * @param string $strNeedle the search term itself (either a literal string OR a regexp value)
+		 * @param integer $intOffset optional offset value
+		 * @return mixed the position number OR false if not found
+		 */
+		public static function StringPosition($strHaystack, &$strNeedle, $intOffset = null) {
+			if ((strlen($strNeedle) >= 3) &&
+				(QString::FirstCharacter($strNeedle) == '/') &&
+				(QString::LastCharacter($strNeedle) == '/')) {
+				$arrMatches = array();
+				preg_match_all($strNeedle, $strHaystack, $arrMatches, null, $intOffset);
+				if (is_array($arrMatches) && array_key_exists(0, $arrMatches)) {
+					$arrMatches = $arrMatches[0];
+				} else
+					return false;
+				if (array_key_exists(0, $arrMatches)) {
+					$strNeedle = $arrMatches[0];
+				} else
+					return false;
+			}
+
+			if (is_null($intOffset)) {
+				return strpos($strHaystack, $strNeedle);
+			} else {
+				return strpos($strHaystack, $strNeedle, $intOffset);
+			}
+		}
+
+		/**
+		 * A better version of strrpos which also allows for the use of RegExp-based matching
+		 * @param $strHaystack the text content to search through
+		 * @param $strNeedle either a plain-text item or a regexp pattern item to search for - if regexp used, this will update as the actual string of the content found
+		 * @param $intOffset optional position offset
+		 * @return mixed the position number OR false if not found
+		 */
+		public static function StringReversePosition($strHaystack, &$strNeedle, $intOffset = null) {
+			if ((strlen($strNeedle) >= 3) &&
+				(QString::FirstCharacter($strNeedle) == '/') &&
+				(QString::LastCharacter($strNeedle) == '/')) {
+				$arrMatches = array();
+				preg_match_all($strNeedle, $strHaystack, $arrMatches);
+				$arrMatches = $arrMatches[0];
+				if (count($arrMatches)) {
+					$strNeedle = $arrMatches[count($arrMatches) - 1];
+				} else
+					return false;
+			}
+			
+			if (is_null($intOffset)) {
+				return strrpos($strHaystack, $strNeedle);
+			} else {
+				return strrpos($strHaystack, $strNeedle, $intOffset);
+			}
+		}
 	}
 ?>
