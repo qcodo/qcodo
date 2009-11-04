fdaafca6b8f637ecca50a96c3ba299e785bdf700
Mike Ho <mike@quasidea.com>
2009-11-03 21:32:56 -0800

    packaging for 0.4.4 release

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.4.3');
+	define('QCODO_VERSION', '0.4.4');
 
 	// Preload Required Framework Classes
 	require(__QCODO_CORE__ . '/framework/QBaseClass.class.php');
