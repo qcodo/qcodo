2da37958fe72816442f149010158d1c8a774b861
Marcos Daniel Sánchez <marcos@marcos-desktop.(none)>
2009-12-01 09:44:31 -0300

    fixes typo

--- includes/qcodo/_core/codegen/QCodeGenBase.class.php
+++ includes/qcodo/_core/codegen/QCodeGenBase.class.php
@@ -263,7 +263,7 @@
 			// Go through standard templates first
 			$objDirectory = opendir($strTemplatePath);
 			while ($strModuleName = readdir($objDirectory))
-				if ((!in_array(strtolower($strModuleName),array_map('strtolower', QCodegen::$DirectoriesToExcludeArray))) &&
+				if ((!in_array(strtolower($strModuleName),array_map('strtolower', QCodeGen::$DirectoriesToExcludeArray))) &&
 					is_dir($strTemplatePath . '/' . $strModuleName)) {
 
 					// We're in a valid Module -- look for any _*.tpl template files
@@ -278,7 +278,7 @@
 			if (is_dir($strTemplatePathCustom)) {
 				$objDirectory = opendir($strTemplatePathCustom);
 				while ($strModuleName = readdir($objDirectory))
-					if ((!in_array(strtolower($strModuleName),array_map('strtolower', QCodegen::$DirectoriesToExcludeArray))) &&
+					if ((!in_array(strtolower($strModuleName),array_map('strtolower', QCodeGen::$DirectoriesToExcludeArray))) &&
 						is_dir($strTemplatePathCustom . '/' . $strModuleName)) {
 						$objModuleDirectory = opendir($strTemplatePathCustom . '/' . $strModuleName);
 						while ($strFilename = readdir($objModuleDirectory))
