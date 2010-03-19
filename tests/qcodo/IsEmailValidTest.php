79e2bdf692c528d0e753c305e92a2c22dd157893
Zbyszek Czarnecki <klucznik@produktiva.net>
2010-03-19 20:22:27 +0100

    updated IsEmailValidTest unit test

--- tests/qcodo/IsEmailValidTest.php
+++ tests/qcodo/IsEmailValidTest.php
@@ -15,123 +15,123 @@
         /* Valid Addresses */
 
         public function testIsEmailValidTest_Standard() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test@example.com'));
         }
 		
         public function testIsEmailValidTest_UpperCaseLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('TEST@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('TEST@example.com'));
         }
 
         public function testIsEmailValidTest_NumericLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('1234567890@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('1234567890@example.com'));
         }
 
         public function testIsEmailValidTest_TaggedLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test+test@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test+test@example.com'));
         }
 
         public function testIsEmailValidTest_QmailLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test-test@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test-test@example.com'));
         }
 
         public function testIsEmailValidTest_UnusualCharactersInLocalPart() {
-        	$this->assertTrue(QEmailUtils::IsEmailValid('~@example.com'));
-            $this->assertTrue(QEmailUtils::IsEmailValid('t*est@example.com'));
-            $this->assertTrue(QEmailUtils::IsEmailValid('+1~1+@example.com'));
-            $this->assertTrue(QEmailUtils::IsEmailValid('{_test_}@example.com'));
+        	$this->assertTrue(QEmailServer::IsEmailValid('~@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('t*est@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('+1~1+@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('{_test_}@example.com'));
         }
 
         public function testIsEmailValidTest_QuotedLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('"[[ test ]]"@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('"[[ test ]]"@example.com'));
         }
 
         public function testIsEmailValidTest_AtomisedLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test.test@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test.test@example.com'));
         }
 
         public function testIsEmailValidTest_ObsoleteLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test."test"@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test."test"@example.com'));
         }
 
         public function testIsEmailValidTest_QuotedAtLocalPart() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('"test@test"@example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('"test@test"@example.com'));
         }
 
         public function testIsEmailValidTest_IpDomain() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test@123.123.123.123'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test@123.123.123.123'));
         }
 
         public function testIsEmailValidTest_BracketIpDomain() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test@[123.123.123.123]'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test@[123.123.123.123]'));
         }
 
         public function testIsEmailValidTest_MultipleLabelDomain() {
-            $this->assertTrue(QEmailUtils::IsEmailValid('test@example.example.com'));
-            $this->assertTrue(QEmailUtils::IsEmailValid('test@example.example.example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test@example.example.com'));
+            $this->assertTrue(QEmailServer::IsEmailValid('test@example.example.example.com'));
         }
 
         /* Invalid Addresses */
 
         public function testInIsEmailValidTest_TooLong() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345@example.com'));
         }
 
         public function testInIsEmailValidTest_TooShort() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('@a'));
+            $this->assertFalse(QEmailServer::IsEmailValid('@a'));
         }
 
         public function testInIsEmailValidTest_NoAtSymbol() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test.example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test.example.com'));
         }
 
         public function testInIsEmailValidTest_BlankAtomInLocalPart() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test.@example.com'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test..test@example.com'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('.test@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test.@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test..test@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('.test@example.com'));
         }
 
         public function testInIsEmailValidTest_MultipleAtSymbols() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@test@example.com'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@test@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@@example.com'));
         }
 	
 	    public function testInIsEmailValidTest_QuotedPairs() {
-	        $this->assertFalse(QEmailUtils::IsEmailValid('test\"test@example.com'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test\@test@example.com'));
+	        $this->assertFalse(QEmailServer::IsEmailValid('test\"test@example.com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test\@test@example.com'));
 		}
 
         public function testInIsEmailValidTest_InvalidCharactersInLocalPart() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('-- test --@example.com')); // No spaces allowed in local part
-            $this->assertFalse(QEmailUtils::IsEmailValid('[test]@example.com')); // Square brackets only allowed within quotes
-            $this->assertFalse(QEmailUtils::IsEmailValid('"test"test"@example.com')); // Quotes cannot be nested
-            $this->assertFalse(QEmailUtils::IsEmailValid('()[]\;:,<>@example.com')); // Disallowed Characters
+            $this->assertFalse(QEmailServer::IsEmailValid('-- test --@example.com')); // No spaces allowed in local part
+            $this->assertFalse(QEmailServer::IsEmailValid('[test]@example.com')); // Square brackets only allowed within quotes
+            $this->assertFalse(QEmailServer::IsEmailValid('"test"test"@example.com')); // Quotes cannot be nested
+            $this->assertFalse(QEmailServer::IsEmailValid('()[]\;:,<>@example.com')); // Disallowed Characters
         }
 
         public function testInIsEmailValidTest_DomainLabelTooShort() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@.'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@example.'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@.org'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@.'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@example.'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@.org'));
         }
 
         public function testInIsEmailValidTest_LocalPartTooLong() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('12345678901234567890123456789012345678901234567890123456789012345@example.com')); // 64 characters is maximum length for local part. This is 65.
+            $this->assertFalse(QEmailServer::IsEmailValid('12345678901234567890123456789012345678901234567890123456789012345@example.com')); // 64 characters is maximum length for local part. This is 65.
         }
 
         public function testInIsEmailValidTest_DomainLabelTooLong() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012.com')); // 255 characters is maximum length for domain. This is 256.
+            $this->assertFalse(QEmailServer::IsEmailValid('test@123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012.com')); // 255 characters is maximum length for domain. This is 256.
         }
 
         public function testInIsEmailValidTest_TooFewLabelsInDomain() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@example'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@example'));
         }
 		
         public function testInIsEmailValidTest_DoubleCommasInDomain() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@example..com'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@example..com'));
         }
 
         public function testInIsEmailValidTest_UnpartneredSquareBracketIp() {
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@[123.123.123.123'));
-            $this->assertFalse(QEmailUtils::IsEmailValid('test@123.123.123.123]'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@[123.123.123.123'));
+            $this->assertFalse(QEmailServer::IsEmailValid('test@123.123.123.123]'));
         }
     }
 ?>
