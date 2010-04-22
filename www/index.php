22ad0eb71e079ed8484c08b3ae8870fba226eb51
Mike Ho <mike@quasidea.com>
2010-04-22 11:04:49 -0700

    Cleanup fix from index

--- www/index.php
+++ www/index.php
@@ -1,6 +1,5 @@
 <?php
 	// Include prepend.inc.php to load Qcodo
 	require(dirname(__FILE__) . '/../includes/prepend.inc.php');
-//	QApplication::QcodoInfo();
-print QApplication::GenerateQueryString();
+	QApplication::QcodoInfo();
 ?>
