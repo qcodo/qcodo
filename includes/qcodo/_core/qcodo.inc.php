14c2c5367242531cccbe81833dd64c136ff48ae6
Mike Ho <mike@quasidea.com>
2010-08-27 22:29:24 -0700

    Packaging for 0.4.17 development release

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.4.16');
+	define('QCODO_VERSION', '0.4.17');
 
 	// Preload Required Framework Classes
 	require(__QCODO_CORE__ . '/framework/QBaseClass.class.php');
