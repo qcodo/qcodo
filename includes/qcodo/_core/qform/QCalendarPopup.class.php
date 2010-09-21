735c3c0ac5c06fa2cecf191f9d4af8f9041aeb10
Marcos Sánchez <marcosdsanchez@thinkclear.com.ar>
2010-09-21 00:07:29 -0300

    QCaledarPopup properties

--- includes/qcodo/_core/qform/QCalendarPopup.class.php
+++ includes/qcodo/_core/qform/QCalendarPopup.class.php
@@ -1,9 +1,13 @@
 <?php
-	// This class is meant to be a date-picker.  It will essentially render an uneditable HTML textbox
-	// as well as a calendar icon.  The idea is that if you click on the icon or the textbox,
-	// it will pop up a calendar in a new small window.
-	// * "DateTime" is a Date object for the specified date.
-
+	/**
+	 * This class is meant to be a date-picker.  It will essentially render an
+	 * uneditable HTML textbox as well as a calendar icon.  The idea is that if
+	 * you click on the icon or the textbox,it will pop up a calendar in a new
+	 * small window.
+	 * 
+	 * @property QDateTime $DateTime is a Date object for the specified date.
+	 * @property string CalendarType
+	 */
 	class QCalendarPopup extends QControl {
 		///////////////////////////
 		// Private Member Variables
