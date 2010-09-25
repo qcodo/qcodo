8003699a5e876a6f0f14ed2c25cd1cb24e39fa66
Mike Ho <mike@quasidea.com>
2010-09-25 00:09:01 -0700

    Fixed status code on error/exception for default error handler

--- includes/qcodo/_core/framework/QErrorHandler.class.php
+++ includes/qcodo/_core/framework/QErrorHandler.class.php
@@ -108,10 +108,10 @@
 					return false;
 				}
 			} else {
+				header("HTTP/1.1 500 Internal Server Error");
 				if (defined('ERROR_FRIENDLY_PAGE_PATH') && ERROR_FRIENDLY_PAGE_PATH && !QApplication::$CliMode) {
 					// Reset the Buffer
 					while(ob_get_level()) ob_end_clean();
-					header("HTTP/1.1 500 Internal Server Error");
 					require(ERROR_FRIENDLY_PAGE_PATH);		
 				}
 			}
