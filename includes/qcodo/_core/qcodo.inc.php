fcb76a0165e4acef1a61f2244415bd08b159d91e
Mike Ho <mike@quasidea.com>
2010-12-24 05:37:59 -0800

    Packaging for 0.4.20 stable release

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.4.19');
+	define('QCODO_VERSION', '0.4.20');
 
 	// PHP Minimum Version Supported
 	define('QCODO_PHP_MIN_VERSION', '5.1.2');
