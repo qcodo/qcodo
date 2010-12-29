6783f82f1d3ead286ee5a696970889303c70f9f9
Marcos Sánchez <marcosdsanchez@thinkclear.com.ar>
2010-12-29 11:56:26 -0300

    fix wrong property name. Thanks apselico\!

--- includes/qcodo/_core/qform/QCheckBoxList.class.php
+++ includes/qcodo/_core/qform/QCheckBoxList.class.php
@@ -8,7 +8,7 @@
 	 * displayed to the left or to the right of the checkbox.
 	 * @property integer $CellPadding specified the HTML Table's CellPadding
 	 * @property integer $CellSpacing specified the HTML Table's CellSpacing
-	 * @property integer $RepeatColumn specifies how many columns should be
+	 * @property integer $RepeatColumns specifies how many columns should be
 	 * rendered in the HTML Table
 	 * @property string $RepeatDirection specifies which direction should the
 	 * list go first...
