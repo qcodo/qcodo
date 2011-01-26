aab2390f68b118f95fad1041d52d0483079c90d8
Zbyszek Czarnecki <klucznik@produktiva.net>
2011-01-26 10:39:13 +0100

    new loook of codegen page

--- www/assets/php/_core/codegen.php
+++ www/assets/php/_core/codegen.php
@@ -1,6 +1,36 @@
+<?php require(dirname(__FILE__) . '/../_require_prepend.inc.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Qcodo Development Framework - Code Generator</title>
+	<style type="text/css">@import url("<?php _p(__VIRTUAL_DIRECTORY__ . __CSS_ASSETS__); ?>/_core/corepage.css");</style>
+</head>
+<body>
+	<div id="container">
+		<div id="headerContainer">
+			<div id="headerBorder">
+				<div id="header">
+					<div id="hleft">
+						<span class="hsmall">Qcodo Development Framework <?php _p(QCODO_VERSION); ?></span><br/>
+						<span class="hbig">Code Generator</span>
+					</div>
+					<div id="hright">
+						<b>PHP Version:</b> <?php _p(PHP_VERSION); ?>&nbsp;&nbsp;
+						<b>Server Name:</b> <?php _p($_SERVER['SERVER_NAME']); ?>&nbsp;&nbsp;
+						<b>Qcodo Version:</b> <?php _p(QCODO_VERSION); ?>
+						<br/>
+						<?php if (array_key_exists('OS', $_SERVER)) printf('<b>Operating System:</b> %s;&nbsp;&nbsp;', $_SERVER['OS']); ?>
+						<b>Application:</b> <?php _p($_SERVER['SERVER_SOFTWARE']); ?>
+						<br/>
+						<b>HTTP User Agent:</b> <?php _p($_SERVER['HTTP_USER_AGENT']); ?>
+					</div>
+					<br class="clear"/>
+				</div>
+			</div>	
+		</div>
+			
+		<div id="content">
 <?php
-	require(dirname(__FILE__) . '/../_require_prepend.inc.php');
-
 	// Security check for ALLOW_REMOTE_ADMIN
 	// To allow access REGARDLESS of ALLOW_REMOTE_ADMIN, simply remove the line below
 	QApplication::CheckRemoteAdmin();
@@ -9,18 +39,20 @@
 	// Run CodeGen, using the path to the codegen settings "settings.xml" file
 	// Change below if your settings.xml file is in a different location
 	/////////////////////////////////////////////////////
-	$strSettingsPath = __DEVTOOLS_CLI__ . '/settings/codegen.xml';
+	$strSettingsXmlFilePath = __DEVTOOLS_CLI__ . '/settings/codegen.xml';
 
-	if (!is_file($strSettingsPath)) {
-		_p('The Qcodo Web-based CodeGen tool expects the codegen settings <strong>"codegen.xml"</strong> file to be located at <br/><br/><strong>', false);
-		_p($strSettingsPath);
+	/*if (!file_exists($strSettingsXmlFilePath) || !is_file($strSettingsXmlFilePath)) {
+		_p('<div class="error">', false);
+		_p('The Qcodo Web-based CodeGen tool expects the codegen settings <strong>"codegen.xml"</strong> file to be located at:<br/><strong>', false);
+		_p($strSettingsXmlFilePath);
 		_p('</strong><br/><br/>', false);
-		_p('Either ensure that the <strong>"codegen.xml"</strong> exists there (recommended) or alternatively to update ', false);
-		_p('<strong>' . __FILE__ . '</strong> to reflect the correct path (not as recommended).', false);
+		_p('Either ensure that the <strong>"codegen.xml"</strong> exists there (recommended) or alternatively to update:<br/>', false);
+		_p('<strong>' . __FILE__ . '</strong><br/> to reflect the correct path (not as recommended).</pre>', false);
+		_p('</div></body></html>', false);
 		exit();
-	}
+	}*/
 
-	QCodeGen::Run($strSettingsPath);
+	QCodeGen::Run($strSettingsXmlFilePath);
 
 	function DisplayMonospacedText($strText) {
 		$strText = QApplication::HtmlEntities($strText);
@@ -36,82 +68,36 @@
 	// Output the Page
 	//////////////////
 ?>
-<html>
-	<head>
-		<title>Qcodo Development Framework - Code Generator</title>
-		<style>
-			body { font-family: 'Arial', 'Helvetica', 'sans-serif'; font-size: 14px; margin: 0px; background-color: white;}
-			a:link, a:visited { text-decoration: none; }
-			a:hover { text-decoration: underline; }
+		<?php if ($strErrors = QCodeGen::$RootErrors) { ?>
+			<p><b>The following root errors were reported:</b></p>
+			<pre class="error"><code><?php DisplayMonospacedText($strErrors); ?></code></pre>
+			<p></p>
+		<?php } else { ?>
+			<p><b>CodeGen Settings (as evaluated from <?php _p(QCodeGen::$SettingsFilePath); ?>):</b></p>
+			<pre><code><?php DisplayMonospacedText(QCodeGen::GetSettingsXml()); ?></code></pre>
+			<p></p>
+		<?php } ?>
 
-			.page { padding: 10px; }
-
-			.headingLeft {
-				background-color: #446644;
-				color: #ffffff;
-				padding: 10px 0px 10px 10px;
-				font-family: 'Verdana', 'Arial', 'Helvetica', 'sans-serif';
-				font-size: 18px;
-				font-weight: bold;
-				width: 70%;
-				vertical-align: bottom;
-			}
-			.headingLeftSmall { font-size: 10px; }
-			.headingRight {
-				background-color: #446644;
-				color: #ffffff;
-				padding: 0px 10px 10px 10px;
-				font-family: 'Verdana', 'Arial', 'Helvetica', 'sans-serif';
-				font-size: 10px;
-				width: 30%;
-				vertical-align: bottom;
-				text-align: right;
-			}
-			.title { font-family: 'Verdana', 'Arial', 'Helvetica', 'sans-serif'; font-size: 19px; font-style: italic; color: #330055; }
-
-			.code { background-color: #f4eeff; padding: 10px 10px 10px 10px; margin-left: 50px; font-size: 11px; font-family: 'Lucida Console', 'Courier New', 'Courier', 'monospaced';}
-			.code_title { font-family: 'Verdana', 'Arial', 'Helvetica', 'sans-serif'; font-size: 12px; font-weight: bold; }
-		</style>
-	</head>
-	<body>
-
-		<table border="0" cellspacing="0" width="100%">
-			<tr>
-				<td nowrap="nowrap" class="headingLeft"><span class="headingLeftSmall">Qcodo Development Framework <?php _p(QCODO_VERSION); ?><br /></span>Code Generator</div></td>
-				<td nowrap="nowrap" class="headingRight">
-					<b>PHP Version:</b> <?php _p(PHP_VERSION); ?>;&nbsp;&nbsp;<b>Zend Engine Version:</b> <?php _p(zend_version()); ?>;&nbsp;&nbsp;<b>Qcodo Version:</b> <?php _p(QCODO_VERSION); ?><br />
-					<?php if (array_key_exists('OS', $_SERVER)) printf('<b>Operating System:</b> %s;&nbsp;&nbsp;', $_SERVER['OS']); ?><b>Application:</b> <?php _p($_SERVER['SERVER_SOFTWARE']); ?>;&nbsp;&nbsp;<b>Server Name:</b> <?php _p($_SERVER['SERVER_NAME']); ?><br />
-					<b>Code Generated:</b> <?php _p(date('l, F j Y, g:i:s A')); ?>
-				</td>
-			</tr>
-		</table>
-		
-		<div class="page">
-			<?php if ($strErrors = QCodeGen::$RootErrors) { ?>
-				<p><b>The following root errors were reported:</b></p>
-				<div class="code"><?php DisplayMonospacedText($strErrors); ?></div>
-				<p></p>
-			<?php } else { ?>
-				<p><b>CodeGen Settings (as evaluated from <?php _p(QCodeGen::$SettingsFilePath); ?>):</b></p>
-				<div class="code"><?php DisplayMonospacedText(QCodeGen::GetSettingsXml()); ?></div>
-				<p></p>
-			<?php } ?>
-
-			<?php foreach (QCodeGen::$CodeGenArray as $objCodeGen) { ?>
-				<p><b><?php _p($objCodeGen->GetTitle()); ?></b></p>
-				<div class="code"><span class="code_title"><?php _p($objCodeGen->GetReportLabel()); ?></span><br/><br/>
-					<?php DisplayMonospacedText($objCodeGen->GenerateAll()); ?>
-					<?php if ($strErrors = $objCodeGen->Errors) { ?>
-						<p class="code_title">The following errors were reported:</p>
-						<?php DisplayMonospacedText($objCodeGen->Errors); ?>
-					<?php } ?>
-				</div><p></p>
-			<?php } ?>
+		<?php foreach (QCodeGen::$CodeGenArray as $objCodeGen) { ?>
+			<p><b><?php _p($objCodeGen->GetTitle()); ?></b></p>
+			<span class="code_title"><?php _p($objCodeGen->GetReportLabel()); ?></span>
+			<pre><code><?php DisplayMonospacedText($objCodeGen->GenerateAll()); ?></code></pre>
 			
-			<?php foreach (QCodeGen::GenerateAggregate() as $strMessage) { ?>
-				<p><b><?php _p($strMessage); ?></b></p>
+			<?php if ($strErrors = $objCodeGen->Errors) { ?>
+			<p class="code_title">The following errors were reported:</p>
+			<pre class="error"><code><?php DisplayMonospacedText($objCodeGen->Errors); ?></code></pre>
 			<?php } ?>
+		
+		<?php } ?>
+		
+		<?php foreach (QCodeGen::GenerateAggregate() as $strMessage) { ?>
+			<p><b><?php _p($strMessage); ?></b></p>
+		<?php } ?>
 
-		</div>				
-	</body>
+		</div>
+		<div id="footer">
+			<b>Code Generated:</b> <?php _p(date('l, F j Y, g:i:s A')); ?>
+		</div>
+	</div>
+</body>
 </html>
