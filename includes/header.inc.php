048f7e936a27109f8e3ee32d78676373c1d58000
Zbyszek Czarnecki <klucznik@produktiva.net>
2011-02-02 21:05:25 +0100

    fix reintroduced typo to header and footer

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,5 +1,5 @@
 <?php
-	// This example header.inc.php is intended to be modfied for your application.
+	// This example header.inc.php is intended to be modified for your application.
 ?>
 <!DOCTYPE html>
 <html>
