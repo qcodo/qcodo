06948a52c1605ae28fa56a6bc236e62c1eea9c86
Mike Ho <mike@quasidea.com>
2018-07-01 12:45:27 -0700

    Schema-based codegen

---
+++ cli/codegen-schema.cli.php
@@ -0,0 +1,115 @@
+<?php
+	// Setup the Parameters for codegen
+	$objParameters = new QCliParameterProcessor('codegen-schema', 'Qcodo ORM Model Code Generator for JSON Schemas v' . QCODO_VERSION);
+
+	// Add new default parameter -- db index
+	$objParameters->AddDefaultParameter('swagger_path', QCliParameterType::Path, 'the path to the Swagger file which has Schema definitions being codegenned');
+
+	$objParameters->Run();
+	if (!is_file($path = $objParameters->GetDefaultValue('swagger_path'))) {
+		print ("error: swagger file not found: " . $path . "\r\n");
+		exit(1);
+	}
+
+	$swaggerText = file_get_contents($path);
+
+	if (!is_dir($schemaPath = __APPLICATION__ . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR . 'Schema')) QApplicationBase::MakeDirectory($schemaPath, 0777);
+	if (!is_dir($schemaGeneratedPath = __APPLICATION__ . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR . 'Schema' . DIRECTORY_SEPARATOR . 'generated')) QApplicationBase::MakeDirectory($schemaGeneratedPath, 0777);
+
+
+
+	function GetPhpDocPropertyForProperty($property) {
+		$ref = '$ref';
+		if (isset($property->$ref)) return str_replace('#/definitions/', null, $property->$ref) . '';
+
+		switch ($property->type) {
+			case 'array':
+				return GetPhpDocPropertyForProperty($property->items) . '[]';
+			case 'integer':
+				return 'integer';
+			case 'boolean':
+				return 'boolean';
+			case 'number':
+				return 'float';
+			case 'string':
+				if (isset($property->format)) {
+					switch ($property->format) {
+						case 'date':
+							return 'QDateTime';
+						case 'date-time':
+							return 'QDateTime';
+					}
+				}
+				return 'string';
+			default:
+				throw new Exception('Unhandled GetPhpDocPropertyForProperty: ' . $property->type);
+		}
+	}
+
+	function GetModelDefinitionForProperty($property) {
+		$ref = '$ref';
+		if (isset($property->$ref)) return "'" . str_replace('#/definitions/', null, $property->$ref) . '' . "'";
+
+		switch ($property->type) {
+			case 'array':
+				return "array('" . GetPhpDocPropertyForProperty($property->items) . "')";
+			case 'integer':
+				return "'integer'";
+			case 'boolean':
+				return "'boolean'";
+			case 'number':
+				return "'float'";
+			case 'string':
+				if (isset($property->format)) {
+					switch ($property->format) {
+						case 'date':
+							return "'date'";
+						case 'date-time':
+							return "'datetime'";
+					}
+				}
+				return "'string'";
+			default:
+				throw new Exception('Unhandled GetModelDefinitionForProperty: ' . $property->type);
+		}
+	}
+
+	$swagger = json_decode($swaggerText);
+	$templateClass = file_get_contents(dirname(__FILE__) . '/template-schema-class.txt');
+	$templateGenerated = file_get_contents(dirname(__FILE__) . '/template-schema-generated.txt');
+	foreach ($swagger->definitions as $schemaName => $schema) {
+		$comment = array();
+		$model = array();
+
+		foreach ($schema->properties as $propertyName => $property) {
+			if (isset($property->description) && $property->description)
+				$comment[] = sprintf('	 * @property %s $%s %s', GetPhpDocPropertyForProperty($property), ucfirst($propertyName), $property->description);
+			else
+				$comment[] = sprintf('	 * @property %s $%s', GetPhpDocPropertyForProperty($property), ucfirst($propertyName));
+			$model[] = sprintf('			\'%s\' => %s,', $propertyName, GetModelDefinitionForProperty($property));
+		}
+
+		$rendered = sprintf($templateGenerated,
+			QApplicationBase::$application->rootNamespace,
+			ucfirst($schemaName) . 'Gen',
+			implode("\r\n", $comment),
+			ucfirst($schemaName) . 'Gen',
+			implode("\r\n", $model),
+			ucfirst($schemaName),
+			ucfirst($schemaName)
+		);
+
+		file_put_contents($schemaGeneratedPath . DIRECTORY_SEPARATOR . ucfirst($schemaName) . 'Gen.php', $rendered);
+
+		$rendered = sprintf($templateClass,
+			QApplicationBase::$application->rootNamespace,
+			QApplicationBase::$application->rootNamespace,
+			ucfirst($schemaName),
+			ucfirst($schemaName) . 'Gen',
+			ucfirst($schemaName),
+			ucfirst($schemaName) . 'Gen'
+		);
+
+		$path = $schemaPath . DIRECTORY_SEPARATOR . ucfirst($schemaName) . '.php';
+		if (!file_exists($path)) file_put_contents($path, $rendered);
+	}
---
+++ includes/qcodo/_core/framework/QJsonBaseClass.class.php
@@ -0,0 +1,248 @@
+<?php
+	abstract class QJsonBaseClass extends QBaseClass {
+		/**
+		 * @var mixed[] contains the values of each property
+		 */
+		protected $mixPropertiesDictionary = array();
+		protected static $_Model;
+
+		public static function JsonDecode($mixJson) {
+			throw new QCallerException('JsonDecode() Not Implemented: ' . get_called_class());
+		}
+
+		protected static function JsonDecodeForClass($strClassName, $mixJson) {
+			$objToReturn = new $strClassName();
+
+			if ($mixJson instanceof stdClass) {
+				$objJson = $mixJson;
+			} else {
+				$objJson = json_decode($mixJson);
+			}
+
+			if (!$objJson) return null;
+
+			// Go Through Each Property in the Model
+			foreach ($objToReturn::$_Model as $strName => $mixType) {
+
+				// If we have defined this Property in the JSON...
+				if (isset($objJson->$strName)) {
+
+					// Is it NULL?
+					if (is_null($objJson->$strName)) {
+						$objToReturn->mixPropertiesDictionary[$strName] = null;
+
+					// Is the TYPE: Array?
+					} else if (is_array($mixType)) {
+						$objToReturn->mixPropertiesDictionary[$strName] = self::JsonDecodeArrayForType($mixType[0], $objJson->$strName);
+
+					// Otherwise, operate based on TYPE
+					} else switch ($mixType) {
+						case 'boolean':
+						case 'date':
+						case 'datetime':
+						case 'float':
+						case 'integer':
+						case 'string':
+							$objToReturn->mixPropertiesDictionary[$strName] = $objJson->$strName;
+							break;
+
+						default:
+							if (!is_subclass_of($mixType, 'QJsonClass')) throw new QCallerException('Unsupported Type for ' . $strClassName . '::' . $strName . ': ' . $mixType);
+							$objToReturn->mixPropertiesDictionary[$strName] = $mixType::JsonDecode($objJson->$strName);
+							break;
+					}
+				}
+			}
+
+			return $objToReturn;
+		}
+
+		public static function JsonDecodeArrayForType($mixType, $mixJson) {
+			if (is_array($mixJson)) {
+				$objJsonArray = $mixJson;
+			} else if (is_object($mixJson) && (get_class($mixJson) == 'stdClass')) {
+				$objJsonArray = array();
+				foreach ($mixJson as $mixKey => $mixValue) $objJsonArray[$mixKey] = $mixValue;
+			} else {
+				$objJsonArray = json_decode($mixJson);
+			}
+
+			if (is_null($objJsonArray)) return null;
+			if (!is_array($objJsonArray)) throw new QCallerException('JsonDecodeArray() cannot decode non-array value');
+
+			if (is_array($mixType)) {
+				$arrToReturn = new ArrayObject();
+				foreach ($objJsonArray as $objJson) $arrToReturn[] = self::JsonDecodeArrayForType($mixType[0], $objJson);
+
+			} else switch ($mixType) {
+				case 'boolean':
+				case 'date':
+				case 'datetime':
+				case 'float':
+				case 'integer':
+				case 'string':
+					$arrToReturn = new ArrayObject($objJsonArray);
+					break;
+
+				default:
+					$arrToReturn = new ArrayObject();
+					if (!is_subclass_of($mixType, 'QJsonClass')) throw new Exception('JsonDecodeArray() cannot decode Unsupported Type: ' . $mixType);
+					foreach ($objJsonArray as $objJson) $arrToReturn[] = $mixType::JsonDecode($objJson);
+					break;
+			}
+
+			return $arrToReturn;
+		}
+
+		public function JsonEncode() {
+			return json_encode($this->GetJsonHelper());
+		}
+
+		public static function JsonEncodeArray($objArray) {
+			if (is_null($objArray)) return json_encode(null);
+
+			if (!is_array($objArray) && !($objArray instanceof ArrayObject)) throw new QCallerException('Cannot GetJsonForArray() on a non-array parameter');
+
+			$objArray = self::GetJsonArrayHelper($objArray);
+			if ($objArray instanceof ArrayObject) $objArray = $objArray->GetArrayCopy();
+			return json_encode($objArray);
+		}
+
+		protected function GetJsonHelper() {
+			$objToExport = $this->mixPropertiesDictionary;
+
+			foreach ($objToExport as $mixIndex => $mixValue) {
+				if ($mixValue instanceof QJsonClass) {
+					$objToExport[$mixIndex] = $mixValue->GetJsonHelper();
+				} else if ($mixValue instanceof ArrayObject) {
+					$objToExport[$mixIndex] = self::GetJsonArrayHelper($mixValue->getArrayCopy());
+				} else if (is_array($mixValue)) {
+					$objToExport[$mixIndex] = self::GetJsonArrayHelper($mixValue);
+				}
+			}
+
+			return $objToExport;
+		}
+
+		protected static function GetJsonArrayHelper($objArray) {
+			foreach ($objArray as $mixIndex => $mixValue) {
+				if ($mixValue instanceof QJsonClass) {
+					$objArray[$mixIndex] = $mixValue->GetJsonHelper();
+				} else if ($mixValue instanceof ArrayObject) {
+					$objArray[$mixIndex] = self::GetJsonArrayHelper($mixValue->getArrayCopy());
+				} else if (is_array($mixValue)) {
+					$objArray[$mixIndex] = self::GetJsonArrayHelper($mixValue);
+				}
+			}
+
+			return $objArray;
+		}
+
+		public function __get($strName) {
+			$strIndex = strtolower(substr($strName, 0, 1)) . substr($strName, 1);
+
+			if (array_key_exists($strIndex, $this::$_Model)) {
+				if (array_key_exists($strIndex, $this->mixPropertiesDictionary)) {
+					switch ($this::$_Model[$strIndex]) {
+						case 'date':
+							if (!$this->mixPropertiesDictionary[$strIndex]) return null;
+							$dttToReturn = new QDateTime($this->mixPropertiesDictionary[$strIndex]);
+							$dttToReturn->SetTime(null, null, null);
+							return $dttToReturn;
+
+						case 'datetime':
+							if (!$this->mixPropertiesDictionary[$strIndex]) return null;
+							return new QDateTime($this->mixPropertiesDictionary[$strIndex]);
+
+						default:
+							return $this->mixPropertiesDictionary[$strIndex];
+					}
+				} else
+					return null;
+			} else {
+				try {
+					return parent::__get($strName);
+				} catch (QCallerException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+			}
+		}
+
+		public function __set($strName, $mixValue) {
+			$strIndex = strtolower(substr($strName, 0, 1)) . substr($strName, 1);
+
+			if (array_key_exists($strIndex, $this::$_Model)) {
+				// Special Case for NULL
+				if (is_null($mixValue)) {
+					$this->mixPropertiesDictionary[$strIndex] = null;
+					return null;
+				}
+
+				// Otherwise, we set based on the definition
+				$mixType = $this::$_Model[$strIndex];
+
+				if (is_array($mixType)) {
+					if (is_array($mixValue))
+						$this->mixPropertiesDictionary[$strIndex] = new ArrayObject($mixValue);
+					else if ($mixValue instanceof ArrayObject)
+						$this->mixPropertiesDictionary[$strIndex] = $mixValue;
+					else
+						throw new QCallerException('Invalid Array Object: ' . $strName);
+					return $mixValue;
+
+				} else switch ($mixType) {
+					case 'boolean':
+						$this->mixPropertiesDictionary[$strIndex] = QType::Cast($mixValue, QType::Boolean);
+						return $mixValue;
+
+					case 'date':
+						if ($mixValue instanceof QDateTime) {
+							$this->mixPropertiesDictionary[$strIndex] = $mixValue->ToString('YYYY-MM-DD');
+						} else if (!$mixValue) {
+							$this->mixPropertiesDictionary[$strIndex] = null;
+						} else {
+							$this->mixPropertiesDictionary[$strIndex] = $mixValue;
+						}
+						return $mixValue;
+
+					case 'datetime':
+						if ($mixValue instanceof QDateTime) {
+							$this->mixPropertiesDictionary[$strIndex] = $mixValue->ToString('YYYY-MM-DDThhhh:mm:sstttttt');
+						} else if (!$mixValue) {
+							$this->mixPropertiesDictionary[$strIndex] = null;
+						} else {
+							$this->mixPropertiesDictionary[$strIndex] = $mixValue;
+						}
+						return $mixValue;
+
+					case 'float':
+						$this->mixPropertiesDictionary[$strIndex] = QType::Cast($mixValue, QType::Float);
+						return $mixValue;
+
+					case 'integer':
+						$this->mixPropertiesDictionary[$strIndex] = QType::Cast($mixValue, QType::Integer);
+						return $mixValue;
+
+					case 'string':
+						$this->mixPropertiesDictionary[$strIndex] = $mixValue;
+						return $mixValue;
+
+					default:
+						if ($mixValue instanceof $this::$_Model[$strIndex]) {
+							$this->mixPropertiesDictionary[$strIndex] = $mixValue;
+							return $mixValue;
+						}
+
+						throw new QCallerException('Mismatched DataType for field [' . get_called_class() . '::'. $strIndex . ']: ' . $this::$_Model[$strIndex]);
+				}
+			} else {
+				try {
+					return parent::__set($strName, $mixValue);
+				} catch (QCallerException $objExc) {
+					$objExc->IncrementOffset();
+					throw $objExc;
+				}
+			}
+		}
+	}
