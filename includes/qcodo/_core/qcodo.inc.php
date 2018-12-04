43ae2208bd876a5f6308c0b17d21d8b445ffdf28
Mike Ho <mike@quasidea.com>
2018-12-04 13:31:59 -0800

    Added getallheaders polyfill from Ralph Khattar

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -85,3 +85,64 @@
 	QApplicationBase::$ClassFile['qpdodatabase'] = __QCODO_CORE__ . '/database/QPdoDatabase.class.php';
 
 	QApplicationBase::$ClassFile['qjsonbaseclass'] = __QCODO_CORE__ . '/framework/QJsonBaseClass.class.php';
+
+	// Polyfill for getallheaders()
+	if (!function_exists('getallheaders')) {
+		/**
+		 * Get all HTTP header key/values as an associative array for the current request.
+		 *
+		 * The MIT License (MIT)
+		 *
+		 * Copyright (c) 2014 Ralph Khattar
+		 *
+		 * Permission is hereby granted, free of charge, to any person obtaining a copy
+		 * of this software and associated documentation files (the "Software"), to deal
+		 * in the Software without restriction, including without limitation the rights
+		 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		 * copies of the Software, and to permit persons to whom the Software i
+		 * furnished to do so, subject to the following conditions:
+		 *
+		 * The above copyright notice and this permission notice shall be included in all
+		 * copies or substantial portions of the Software.
+		 *
+		 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		 * SOFTWARE.
+		 *
+		 * @return string[] The HTTP header key/value pairs.
+		 */
+		function getallheaders() {
+			$headers = array();
+			$copy_server = array(
+				'CONTENT_TYPE'   => 'Content-Type',
+				'CONTENT_LENGTH' => 'Content-Length',
+				'CONTENT_MD5'    => 'Content-Md5',
+			);
+			foreach ($_SERVER as $key => $value) {
+				if (substr($key, 0, 5) === 'HTTP_') {
+					$key = substr($key, 5);
+					if (!isset($copy_server[$key]) || !isset($_SERVER[$key])) {
+						$key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+						$headers[$key] = $value;
+					}
+				} elseif (isset($copy_server[$key])) {
+					$headers[$copy_server[$key]] = $value;
+				}
+			}
+			if (!isset($headers['Authorization'])) {
+				if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+					$headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+				} elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+					$basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+					$headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+				} elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+					$headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+				}
+			}
+			return $headers;
+		}
+	}
