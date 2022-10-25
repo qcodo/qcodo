f0dbca4b1824529edbde6335a681b90f03f35706
Mike Ho <mike@quasidea.com>
2022-10-24 21:36:11 -0700

    Release v0.5.22

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.5.21');
+	define('QCODO_VERSION', '0.5.22');
 
 	// PHP Minimum Version Supported
 	define('QCODO_PHP_MIN_VERSION', '5.3.0');
