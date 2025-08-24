c90db230bb1804e7998409c0f594b89e6e9a08a1
Mike Ho <mike@quasidea.com>
2025-08-23 19:43:31 -0700

    Release v0.6.14

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.6.13');
+	define('QCODO_VERSION', '0.6.14');
 
 	// PHP Minimum Version Supported
 	define('QCODO_PHP_MIN_VERSION', '7.0.0');
