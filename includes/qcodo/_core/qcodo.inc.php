de085c63a667657591cbe071d37fcbd9cd1aa28a
Mike Ho <mike@quasidea.com>
2025-10-26 20:56:50 -0700

    Release v0.6.19

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -31,7 +31,7 @@
 	 */
 
 	// Versioning Information
-	define('QCODO_VERSION', '0.6.18');
+	define('QCODO_VERSION', '0.6.19');
 
 	// PHP Minimum Version Supported
 	define('QCODO_PHP_MIN_VERSION', '7.0.0');
